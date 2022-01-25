6c24599917b4a42a9db793356bf5a5bfdd5cb1b6
Viral Patel <nishilpatel@cloud.upwork.com>
2022-01-25 18:22:34 +0530

    Added the basic folder structure for Unity Framework

---
+++ src/test/java/generator/method_generator.java
@@ -0,0 +1,11 @@
+package generator;
+
+import io.unity.object_generator;
+
+public class method_generator {
+    public static void main(String[] args) {
+        object_generator generator = new object_generator();
+        generator.generate();
+
+    }
+}
